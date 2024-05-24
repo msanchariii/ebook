936a3fd4379a82eb3ac59bd1ce2683c21d4b33d2
Soumi Saha <sahasoumi68@gmail.com>
2024-05-25 01:24:01 +0530

    added favicon and navbar correction

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -44,7 +44,7 @@ export default function Example() {
 
     return (
         <div className="relative w-full bg-white">
-            <div className="mx-auto flex max-w-7xl items-center justify-between px-2 py-2 sm:px-6 lg:px-8 shadow">
+            <div className="mx-auto flex items-center justify-between px-2 py-2 sm:px-6 lg:px-8 shadow">
                 <div className="inline-flex items-center space-x-2">
                     <span>
                         <Image
