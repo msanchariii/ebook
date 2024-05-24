fcb6df906ccf202f30ca1bfbb0c3edd559cfc0f2
Sanchari Mandal <113378204+Hul0@users.noreply.github.com>
2024-05-24 16:39:16 +0530

    style change in /ebooks

--- src/app/(book-types)/ebooks/page.tsx
+++ src/app/(book-types)/ebooks/page.tsx
@@ -1,11 +1,32 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
-function page() {
+async function page() {
+    const response = await fetch(`${process.env.BASE_URL}/api/get-all-books`);
+    const responseData = await response.json();
+    let books;
+    if (responseData.success) {
+        books = responseData.data;
+    }
     return (
         <div className="p-4">
             <h1 className="text-center text-4xl font-semibold">Books</h1>
-            <div></div>
+            <div>
+                {books &&
+                    books.length > 0 &&
+                    books.map((book: any) => (
+                        <div
+                            key={book._id}
+                            className="bg-yellow-200 w-1/3 p-4 rounded-lg flex flex-col gap-y-2"
+                        >
+                            <h1 className="font-bold">{book.title}</h1>
+                            <p>{book.description}</p>
+                            <Button className="w-20">Buy</Button>
+                        </div>
+                    ))}
+            </div>
         </div>
     );
 }
+
 export default page;
--- src/app/(book-types)/emagazines/page.tsx
+++ src/app/(book-types)/emagazines/page.tsx
@@ -4,7 +4,9 @@ function page() {
     return (
         <div className="p-4">
             <h1 className="text-center text-4xl font-semibold">Magazines</h1>
-            <div></div>
+            <div>
+                <p>No Magazines to display</p>
+            </div>
         </div>
     );
 }
